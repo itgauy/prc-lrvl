66718faf78e4b477a653d94071c7aa00e08f8717
itgauy <torreslian@duck.com>
2024-09-18 13:13:50 +0800

    Registration Authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,25 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     // Register User
     public function register(Request $request) {
         // Validate
-        $request->validate([
+        $fields = $request->validate([
             'username' => ['required', 'max:255'],
-            'email' => ['required', 'max:255', 'email'],
+            'email' => ['required', 'max:255', 'email', 'unique:users'],
             'password' => ['required', 'min:3', 'confirmed']
         ]);
 
-        dd('ok');
-
         // Register
+        $user = User::create($fields);
 
         // Login
-
+        Auth::login($user);
+        
         //Redirect
+        return redirect()->route('home');
     }
 }
