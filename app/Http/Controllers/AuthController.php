61b89033c7e43206e40f20d2b675f5586d2cffb8
itgauy <torreslian@duck.com>
2024-09-19 23:34:15 +0800

    Log in and Log out

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,4 +26,38 @@ class AuthController extends Controller
         //Redirect
         return redirect()->route('home');
     }
+
+    // Login User
+    public function login(Request $request) {
+        // Validate
+        $fields = $request->validate([
+            'email' => ['required', 'max:255', 'email'],
+            'password' => ['required']
+        ]);
+
+        // Try to login the user
+        if(Auth::attempt($fields, $request->remember)) {
+            return redirect()->intended('dashboard');
+        } else {
+            return back()->withErrors(([
+                'failed' => 'The provided credentials do not match our records.'
+            ]));
+        }
+    }
+
+    // Logout User
+    public function logout(Request $request) {
+
+        // Logout the user
+        Auth::logout();
+        
+        // Invalidate user's session
+        $request->session()->invalidate();
+
+        // Regenerate CSRF token
+        $request->session()->regenerateToken();
+
+        // Redirect to home
+        return redirect('/');
+    }
 }
