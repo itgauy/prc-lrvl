7c17031cebbf611fd2dac061856d9d28e356e7a7
itgauy <torreslian@duck.com>
2024-09-24 20:52:56 +0800

    Middleware for auth and guest

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'posts.index')->name('home');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+  Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+  Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::view('/register', 'auth.register')->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::view('/login', 'auth.login')->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('guest')->group(function () {
+  Route::view('/register', 'auth.register')->name('register');
+  Route::post('/register', [AuthController::class, 'register']);
+  
+  Route::view('/login', 'auth.login')->name('login');
+  Route::post('/login', [AuthController::class, 'login']);
+});
